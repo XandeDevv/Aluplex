0ca6f05114ff1c1b8736d0ab9073ec8c93bbe812
Alexandre <alexandrexan2@hotmail.com>
2024-10-16 20:23:08 -0300

    proprietario entity created

--- src/main/java/com/example/Aluguel/de/imoveis/domains/Proprietario.java
+++ src/main/java/com/example/Aluguel/de/imoveis/domains/Proprietario.java
@@ -1,4 +1,14 @@
 package com.example.Aluguel.de.imoveis.domains;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "proprietarios")
 public class Proprietario {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+    private String name;
+    private String email;
+    private String cpf;
+
 }
