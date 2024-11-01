75fbd08d336c02690f83e3f0c8b0a10086c30aea
Alexandre <alexandrexan2@hotmail.com>
2024-11-01 16:47:13 -0300

    criarcontrato and aceitarcontrato almost done

---
+++ back-Aluguel-de-Imoveis/src/main/java/com/example/Aluguel/de/imoveis/repositories/ContratoRepository.java
@@ -0,0 +1,7 @@
+package com.example.Aluguel.de.imoveis.repositories;
+
+import com.example.Aluguel.de.imoveis.domains.Contrato;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ContratoRepository extends JpaRepository<Contrato,Long> {
+}
--- back-Aluguel-de-Imoveis/src/main/java/com/example/Aluguel/de/imoveis/services/ContratoService.java
+++ back-Aluguel-de-Imoveis/src/main/java/com/example/Aluguel/de/imoveis/services/ContratoService.java
@@ -6,6 +6,7 @@ import com.example.Aluguel.de.imoveis.domains.User;
 import com.example.Aluguel.de.imoveis.domains.UserRole;
 import com.example.Aluguel.de.imoveis.dtos.ContratoDto;
 import com.example.Aluguel.de.imoveis.dtos.UserDto;
+import com.example.Aluguel.de.imoveis.repositories.ContratoRepository;
 import com.example.Aluguel.de.imoveis.services.exceptions.ControllerNotFoundException;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,26 +15,30 @@ import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class ContratoService {
+    @Autowired
+    private ContratoRepository contratoRepository;
 
     @Autowired
     private AuthorizationService authorizationService;
     @Transactional
     public ContratoDto criarContrato(ContratoDto contratoDto){
         User user= authorizationService.authenticated();
-        Contrato contrato= new Contrato();
-        BeanUtils.copyProperties(contratoDto,contrato);
+        UserDto userDto= new UserDto(user);
         if(user.hasRole(UserRole.USER)){
-            contrato.setCliente(user);
+            contratoDto.setCliente(userDto);
         }if(user.hasRole(UserRole.PROPRIETARIO)){
-            contrato.setProprietario(user);
+            contratoDto.setProprietario(userDto);
         }else {
             throw new ControllerNotFoundException("Usuario nao habiitado para fazer contratos");
         }
-        contrato.setStatus(ContratoStatus.PENDENTE);
+        contratoDto.setStatus(ContratoStatus.PENDENTE);
+        Contrato contrato= new Contrato();
+        BeanUtils.copyProperties(contratoDto,contrato);
+        contrato=contratoRepository.save(contrato);
         return new ContratoDto(contrato);
     }
     @Transactional
-    public void aceitarContrato(ContratoDto contratoDto) {
+    public ContratoDto aceitarContrato(ContratoDto contratoDto) {
         User user = authorizationService.authenticated();
         UserDto userDto= new UserDto(user);
         if (contratoDto.getCliente() == null && !user.hasRole(UserRole.USER)) {
@@ -49,5 +54,9 @@ public class ContratoService {
             contratoDto.setProprietario(userDto);
         }
         contratoDto.setStatus(ContratoStatus.EM_ANDAMENTO);
+        Contrato contrato= new Contrato();
+        BeanUtils.copyProperties(contratoDto,contrato);
+        contrato=contratoRepository.save(contrato);
+        return new ContratoDto(contrato);
     }
 }
